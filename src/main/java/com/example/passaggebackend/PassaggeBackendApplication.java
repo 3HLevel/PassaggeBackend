d5001bf8f1f76e667a450b29f3c059749c1adab1
Khhhazov <SemenKhazov@gmail.com>
2022-07-16 18:04:35 +0300

    init

---
+++ src/main/java/com/example/passaggebackend/PassaggeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.passaggebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassaggeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassaggeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/passaggebackend/PassaggeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.passaggebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassaggeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
